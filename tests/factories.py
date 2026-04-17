0787c8a23d7b0b4bbe90d06c040652867a957fa7
danielaquaries20 <danielaquaries@gmail.com>
2026-04-17 13:31:07 +0700

    Update ProductFactory with fake data generation

--- tests/factories.py
+++ tests/factories.py
@@ -1,19 +1,3 @@
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# pylint: disable=too-few-public-methods
-
 """
 Test Factory to make fake objects for testing
 """
@@ -21,14 +5,33 @@ import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
-
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
 
     class Meta:
-        """Maps factory to data model"""
-
         model = Product
 
+    # Membuat ID berurutan secara otomatis
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    
+    # Menghasilkan nama produk dan deskripsi acak
+    name = factory.Faker("word")
+    description = factory.Faker("text")
+    
+    # Menghasilkan harga acak antara $0.50 hingga $2000.00
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    
+    # Ketersediaan acak (True atau False)
+    available = FuzzyChoice(choices=[True, False])
+    
+    # Memilih kategori secara acak dari Enum Category yang ada di model
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
